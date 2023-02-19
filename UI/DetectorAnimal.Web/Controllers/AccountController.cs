44d1b28e5a79f45b038b699ef48ffe54f8498433
Alexander <74.71.spartacus@gmail.com>
2023-02-19 23:07:24 +0300

    Создан функционал регистрации пользователя

---
+++ UI/DetectorAnimal.Web/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using DetectorAnimal.Data.Entities;
+using DetectorAnimal.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DetectorAnimal.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        private readonly SignInManager<User> _signInManager;
+
+        
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _userManager.FindByNameAsync(model.UserName) is null)
+                {
+                    if (await _userManager.FindByEmailAsync(model.Email) is null)
+                    {
+                        User user = new()
+                        {
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            UserName = model.UserName,
+                            Email = model.Email
+                        };
+
+                        var result = await _userManager.CreateAsync(user, model.Password);
+
+                        if (result.Succeeded)
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(model.Email, "Такой email уже зарегистрирован.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(model.UserName, "Такой логин уже существует.");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ UI/DetectorAnimal.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DetectorAnimal.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = nameof(FirstName))]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = nameof(LastName))]
+        public string LastName { get; set; }
+        
+        [Required]
+        [MinLength(4)]
+        [MaxLength(12)]
+        [Display(Name = nameof(UserName))]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = nameof(Email))]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string PasswordConfirmed { get; set; }
+    }
+}
