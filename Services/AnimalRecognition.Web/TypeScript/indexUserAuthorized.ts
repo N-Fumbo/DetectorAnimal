3eecebd42eb2e4a98673a81b169e6958e70d5bff
Alexander <74.71.spartacus@gmail.com>
2023-04-13 01:33:11 +0300

    Рефакторинг кода

--- Services/AnimalRecognition.Web/TypeScript/indexUserAuthorized.ts
+++ Services/AnimalRecognition.Web/TypeScript/indexUserAuthorized.ts
@@ -1,8 +1,21 @@
 import $ from 'jquery';
 import preloader from "./preloader";
 import submitRecognitionForm from './form/recognitionForm'
+import ModalWindow from './ModalWindow';
 
 $(() => {
     submitRecognitionForm();
+
+    const modalWindowLogIn = new ModalWindow('#modal_window_recognition');
+    $('#recognition').on('click', function (e) {
+        e.preventDefault();
+        if (modalWindowLogIn !== null) modalWindowLogIn.open();
+    });
+
+    $('#modal_window_recognition .modal_window_close').on('click', function (e) {
+        e.preventDefault();
+        if (modalWindowLogIn !== null) modalWindowLogIn.close();
+    });
+
     preloader(true);
 });
--- Services/AnimalRecognition.Web/wwwroot/js/build/indexUserAuthorized.js
+++ Services/AnimalRecognition.Web/wwwroot/js/build/indexUserAuthorized.js
@@ -12150,8 +12150,20 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const jquery_1 = __importDefault(__webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"));
 const preloader_1 = __importDefault(__webpack_require__(/*! ./preloader */ "./TypeScript/preloader.ts"));
 const recognitionForm_1 = __importDefault(__webpack_require__(/*! ./form/recognitionForm */ "./TypeScript/form/recognitionForm.ts"));
+const ModalWindow_1 = __importDefault(__webpack_require__(/*! ./ModalWindow */ "./TypeScript/ModalWindow.ts"));
 (0, jquery_1.default)(() => {
     (0, recognitionForm_1.default)();
+    const modalWindowLogIn = new ModalWindow_1.default('#modal_window_recognition');
+    (0, jquery_1.default)('#recognition').on('click', function (e) {
+        e.preventDefault();
+        if (modalWindowLogIn !== null)
+            modalWindowLogIn.open();
+    });
+    (0, jquery_1.default)('#modal_window_recognition .modal_window_close').on('click', function (e) {
+        e.preventDefault();
+        if (modalWindowLogIn !== null)
+            modalWindowLogIn.close();
+    });
     (0, preloader_1.default)(true);
 });
 
