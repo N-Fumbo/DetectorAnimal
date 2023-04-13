f597fff62d9e1f5e96d1258651cdda6b95049c35
Alexander <74.71.spartacus@gmail.com>
2023-04-13 20:59:58 +0300

    Поправка стилей. Создание модального окна Privacy

--- Services/AnimalRecognition.Web/TypeScript/indexUserNotAuthorized.js
+++ Services/AnimalRecognition.Web/TypeScript/indexUserNotAuthorized.js
@@ -9,33 +9,37 @@ const preloader_1 = __importDefault(require("./preloader"));
 const registerForm_1 = __importDefault(require("./form/registerForm"));
 const logInForm_1 = __importDefault(require("./form/logInForm"));
 (0, jquery_1.default)(() => {
+    const modalWindowPrivacy = new ModalWindow_1.default('#modal_window_privacy');
+    (0, jquery_1.default)('#privacy').on('click', function (e) {
+        e.preventDefault();
+        modalWindowPrivacy.open();
+    });
+    (0, jquery_1.default)('#modal_window_privacy .modal_window_close').on('click', function (e) {
+        e.preventDefault();
+        modalWindowPrivacy.close();
+    });
     const modalWindowLogIn = new ModalWindow_1.default('#modal_window_login');
     (0, jquery_1.default)('#login').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowLogIn !== null)
-            modalWindowLogIn.open();
+        modalWindowLogIn.open();
     });
     (0, jquery_1.default)('#modal_window_login .modal_window_close').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowLogIn !== null)
-            modalWindowLogIn.close();
+        modalWindowLogIn.close();
     });
     const modalWindowRegister = new ModalWindow_1.default('#modal_window_register');
     (0, jquery_1.default)('#register').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowRegister !== null)
-            modalWindowRegister.open();
+        modalWindowRegister.open();
     });
     (0, jquery_1.default)('#modal_window_register .modal_window_close').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowRegister !== null)
-            modalWindowRegister.close();
+        modalWindowRegister.close();
     });
     const modalWindowDetect = new ModalWindow_1.default('#modal_window_recognition');
     (0, jquery_1.default)('#modal_window_recognition .modal_window_close').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowDetect !== null)
-            modalWindowDetect.close();
+        modalWindowDetect.close();
     });
     (0, registerForm_1.default)();
     (0, logInForm_1.default)();
--- Services/AnimalRecognition.Web/TypeScript/indexUserNotAuthorized.ts
+++ Services/AnimalRecognition.Web/TypeScript/indexUserNotAuthorized.ts
@@ -5,33 +5,45 @@ import submitRegisterForm from './form/registerForm';
 import submitLogInForm from './form/logInForm';
 
 $(() => {
+
+    const modalWindowPrivacy = new ModalWindow('#modal_window_privacy');
+    $('#privacy').on('click', function (e) {
+        e.preventDefault();
+        modalWindowPrivacy.open();
+    });
+
+    $('#modal_window_privacy .modal_window_close').on('click', function (e) {
+        e.preventDefault();
+        modalWindowPrivacy.close();
+    });
+
     const modalWindowLogIn = new ModalWindow('#modal_window_login');
     $('#login').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowLogIn !== null) modalWindowLogIn.open();
+        modalWindowLogIn.open();
     });
 
     $('#modal_window_login .modal_window_close').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowLogIn !== null) modalWindowLogIn.close();
+        modalWindowLogIn.close();
     });
 
     const modalWindowRegister = new ModalWindow('#modal_window_register');
     $('#register').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowRegister !== null) modalWindowRegister.open();
+        modalWindowRegister.open();
     });
 
     $('#modal_window_register .modal_window_close').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowRegister !== null) modalWindowRegister.close();
+        modalWindowRegister.close();
     });
 
     const modalWindowDetect = new ModalWindow('#modal_window_recognition');
 
     $('#modal_window_recognition .modal_window_close').on('click', function (e) {
         e.preventDefault();
-        if (modalWindowDetect !== null) modalWindowDetect.close();
+        modalWindowDetect.close();
     });
 
     submitRegisterForm();
